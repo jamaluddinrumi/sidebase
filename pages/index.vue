9a5a9fdba90e6e529ed63604500298156a41b1eb
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-11-22 17:38:23 +0700

    linting /pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,6 @@ const { data } = await useLazyFetch('/api/progress')
 </script>
 
 <template>
-
   <Head>
     <Title>Progress | INDOMASCOT</Title>
     <Meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
